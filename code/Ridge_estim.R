e35b9aff3b160d041049dd367a088c4db929f07b
alainmbebi <66583407+alainmbebi@users.noreply.github.com>
2020-06-08 12:33:34 +0200

    Update Ridge_estim.R

--- code/Ridge_estim.R
+++ code/Ridge_estim.R
@@ -3,7 +3,7 @@
 # X nxp predictor matrix 
 # lambdaB regularization parameter
 
-Ridge_estim<-function(lambdaB, X, Y){
+Ridge_estim <- function(lambdaB, X, Y){
   Bhat_Ridge= round(t(X)%*%solve(X%*%t(X) + lambdaB*diag(nrow(Y)))%*%Y ,6)
   return(Bhat_Ridge)
 }
