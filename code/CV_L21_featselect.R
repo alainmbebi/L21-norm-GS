aa8e5ad1ed548e36f6d147441dca7806b090fb71
alainmbebi <66583407+alainmbebi@users.noreply.github.com>
2020-06-08 11:34:26 +0200

    Create CV_L21_featselect.R

---
+++ code/CV_L21_featselect.R
@@ -0,0 +1,57 @@
+# Function to implement Cross Validation for L21_featselect
+# Y nxs response matrix
+# X nxp predictor matrix 
+# lam vector of positive tuning parameters. Default grid is {10^seq(-3, 3, 1)}.
+# kfold is the number of folds for cross validation.
+# The returned list includes
+# lam--> optimal tuning parameter.
+# min.error--> minimum average cross validation error for optimal parameters.
+# avg.error--> average cross validation error across all folds.
+
+
+CV_L21_featselect = function(Y, X, lam, kfold=5) {
+  
+  lam = sort(lam)
+  # initialize
+  CV_errors = array(0, c(length(lam), kfold))
+  
+  # designate folds and shuffle -- ensures randomized folds
+  n = nrow(Y)
+  ind = sample(n)
+  
+  # parse data into folds and perform CV
+  
+  for (k in 1:kfold) {
+    
+    leave.out = ind[ (1 + floor((k - 1)*n/kfold)):floor(k*n/kfold) ]
+    # training set
+    Y.train = Y[-leave.out,]
+    X.train = X[-leave.out,]
+    
+    # validation set
+    Y.valid = Y[leave.out, ]
+    X.valid = X[leave.out, ]
+        
+    # loop over all tuning parameters
+    for (i in 1:length(lam)) {
+      
+      # compute the penalized regression matrix estimate with the training set
+      B_hat.train=L21_featselect(lam[i], X.train, Y.train) 
+      
+      # compute the CV errors
+      CV_errors[i,k] = CV_errors[i,k] + mean((Y.valid-X.valid%*%B_hat.train)^2)
+    }
+    
+    
+  }
+  
+  # determine optimal tuning parameters
+  AVG = apply(CV_errors, 2, mean)
+  best_lam = lam[which.min(AVG)]
+  error = min(AVG)
+  
+  
+  # return best lam and other meaningful values
+  return(list(lam = best_lam, min.error = error, avg.error = AVG, cv.error = CV_errors))
+  
+}
