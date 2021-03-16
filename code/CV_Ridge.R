1b113aa803d2d46838a5b77a25a7defb5185f326
Alain Mbebi <66583407+alainmbebi@users.noreply.github.com>
2021-03-16 18:33:06 +0100

    Update CV_Ridge.R

--- code/CV_Ridge.R
+++ code/CV_Ridge.R
@@ -11,52 +11,105 @@
                                                    #start
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-CV_Ridge <- function(Y, X, lambdaB, kfold=5) {
-  
+#function for cross validation in multiple ridge
+multi.elasnet.cv <-function(X,Y, lambdaB, kfold=3, silent=TRUE){
+  set.seed(123)
+  n.out = nrow(Y)
+  K.out = kfold
+  d.out = ceiling(n.out/K.out)
+  set.seed(123)
+  i.mix.out = sample(1:n.out)
+  folds.out = vector(mode="list", length=K.out) 
+  for (k in 1:(K.out-1)) {
+    folds.out[[k]] = i.mix.out[((k-1)*d.out+1):(k*d.out)]
+  }
+  folds.out[[K.out]] = i.mix.out[((K.out-1)*d.out+1):n.out]
+  # Setting the tuning parameter values for RR
   lambdaB = sort(lambdaB)
-  # initialize
-  CV_errors = array(0, c(length(lambdaB), kfold))
+  #This vector (CV_errors) will contain the prediction errors for each value of tuning parameter
+  #lambdaOmega on the rows and lambdaB on the columns, when fitting the model.
   
-  n = nrow(Y)
-  ind = sample(n)
+  CV_errors.out = rep(NA, K.out)
+  CV_errors.val.out = rep(list(CV_errors.out), K.out)
   
-  # parse data into folds and perform CV
+  CV_corr.out = matrix(NA, nrow=ncol(Y), ncol=ncol(Y))
+  CV_corr.val.out = rep(list(CV_corr.out), K.out)
   
-  for (k in 1:kfold) {
+  #opt = list()
+  opt.i=c()
+  opt.lamB.out = c()
+  
+  for (l in 1:K.out) {
+    cat("outer Fold",l,"\n")
+    i.tr.out = unlist(folds.out[-k])
+    i.val.out = folds.out[[k]]
     
-    leave.out = ind[ (1 + floor((k - 1)*n/kfold)):floor(k*n/kfold) ]
+    X.tr.out = X[i.tr.out,]   # training predictors
+    Y.tr.out = Y[i.tr.out,]    # training responses
     
-    # training set
-    Y.train = Y[-leave.out,]
-    X.train = X[-leave.out,]
+    X.val.out = X[i.val.out,] # validation predictors
+    Y.val.out = Y[i.val.out,]  # validation responses
     
-    # validation set
-    Y.valid = Y[leave.out, ]
-    X.valid = X[leave.out, ]
+    #Inner loop for hyper params  
+    n.iner = nrow(Y.tr.out)
+    K.iner = 3
+    d.iner = ceiling(n.iner/K.iner)
+    set.seed(123)
+    i.mix.iner = sample(1:n.iner)
+    folds.iner = vector(mode="list", length=K.iner) 
+    for (k in 1:(K.iner-1)) {
+      folds.iner[[k]] = i.mix.iner[((k-1)*d.iner+1):(k*d.iner)]
+    }
+    folds.iner[[K.iner]] = i.mix.iner[((K.iner-1)*d.iner+1):n.iner]  
     
-    
-    # loop over all tuning parameters
-    for (i in 1:length(lambdaB)) {
+    #CV_errors will contain the prediction errors for each tuning parameters when fitting the model.
+    CV_errors.iner = rep(NA, length(lambdaB))
+    CV_errors.val.iner = rep(list(CV_errors.iner), K.iner)
+
+    for (k in 1:K.iner) {
+      cat("inner Fold",k,"\n")
       
-      # compute the Ridge regression matrix estimate with the training set
-      B_hat.train=Ridge_estim(lambdaB[i], X.train, Y.train)   
+      i.tr.iner = unlist(folds.iner[-k])
+      i.val.iner = folds.iner[[k]]
       
-      # compute the CV errors
-      CV_errors[i,k] = CV_errors[i,k] + mean((Y.valid-X.valid%*%B_hat.train)^2)
+      X.tr.iner  = X.tr.out[i.tr.iner,]   # training predictors in the inner loop 
+      Y.tr.iner  = Y.tr.out[i.tr.iner,]    # training responses in the inner loop 
+      X.val.iner = X.tr.out[i.val.iner,] # validation predictors in the inner loop 
+      Y.val.iner = Y.tr.out[i.val.iner,]  # validation responses in the inner loop 
+      
+      # Next, use the function JointEstim on the training inner data to get the 
+      # L21-joint regression coefficients for all pairs of tuning parameter values in
+      # lambdaO and lambdaB.
+      # loop over all tuning parameters
+      
+        for(i in 1:length(lambdaB)){
+          # compute the penalized regression matrix estimate with the training set
+          B_hat.tr.iner = multi.elasnet(X.tr.iner, Y.tr.iner, lambdaB[i]) 
+          CV_errors.val.iner[[k]][i] = mean(mse.matrix(X.val.iner%*%B_hat.tr.iner,Y.val.iner))#eventually use the RV coef
+        }
+      
+      idx.min=which.min((sapply(CV_errors.val.iner, min)))
+      opt = which.min(CV_errors.val.iner[[idx.min]])      
+      opt.i[k] = opt
+      opt.lamB.out[k] = lambdaB[opt.i[k]]
+      
     }
     
-    
+    #Now with the optimal tuning parameters from the inner loop
+    #we validate on the left aside fold in the outer loop
+    # compute the penalized regression matrix estimate with the training set
+        B_hat.tr.out = multi.elasnet(X.tr.out, Y.tr.out, opt.lamB.out[l]) 
+        CV_errors.val.out[[l]] = mse.matrix(X.val.out%*%B_hat.tr.out, Y.val.out, na.rm = TRUE)#needed to compute the average metrics over all val folds
+        CV_corr.val.out[[l]] = cor(X.val.out%*%B_hat.tr.out, Y.val.out)
   }
   
-  # determine optimal tuning parameters
-  AVG = apply(CV_errors, 1, mean)
-  best_lam = lambdaB[which.min(AVG)]
-  error = min(AVG)
-  
-  
-  # return the optimal tuning parameter and other meaningful values
-  return(list(lambdaB = best_lam, min.error = error, avg.error = AVG, cv.error = CV_errors))
-  
+  best.idx.out=which.min((sapply(CV_errors.val.out, min)))
+  best.lamB.final = opt.lamB.out[best.idx.out]
+  #output on all data
+  B_hat_final = multi.elasnet(X, Y, best.lamB.final) 
+  # return best lambdaB and other meaningful values
+  return(list(best.lamB.final=best.lamB.final, B_hat_final=B_hat_final, cv_corr=CV_corr.val.out, cv.err=CV_errors.val.out)) 
+   
 }
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
